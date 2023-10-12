68ca2f13745b0232ee33444e031c1073e4961b60
Thanyakorn Thalaengdi <68809500+pisenuta@users.noreply.github.com>
2023-10-12 08:36:40 +0700

    update: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Anuphan: [400, 700],
+        },
+      },
+    ],
+  ],
   css: ['vuetify/lib/styles/main.sass', '~/assets/global.css'],
   build: {
     transpile: ['vuetify'],
@@ -10,5 +20,14 @@ export default defineNuxtConfig({
     head: {
       titleTemplate: '%s | ASAP',
     },
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
   },
 })
